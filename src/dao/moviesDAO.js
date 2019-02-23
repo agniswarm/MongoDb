5489079ca33c0e4b076d849bb81fd7f42073af31
Agniswar Mandal <agniswarmandal@gmail.com>
2019-02-23 20:54:16 +0530

    Updated upto delete ticket

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -198,6 +198,9 @@ export default class MoviesDAO {
       sortStage,
       // TODO Ticket: Faceted Search
       // Add the stages to queryPipeline in the correct order.
+      skipStage,
+      limitStage,
+      facetStage
     ]
 
     try {
@@ -261,7 +264,7 @@ export default class MoviesDAO {
 
     // TODO Ticket: Paging
     // Use the cursor to only return the movies that belong on the current page
-    const displayCursor = cursor.limit(moviesPerPage)
+    const displayCursor = cursor.limit(moviesPerPage).skip(moviesPerPage * page).limit(moviesPerPage)
 
     try {
       const moviesList = await displayCursor.toArray()
@@ -299,6 +302,22 @@ export default class MoviesDAO {
         {
           $match: {
             _id: ObjectId(id)
+          }
+        },
+        {
+          $lookup: {
+            from: 'comments',
+            let: { id: '$_id' },
+            pipeline: [{
+              $match: {
+                $expr: {
+                  $eq: ['$movie_id', '$$id']
+                }
+              }
+            },{
+              $sort:{'date':-1}
+            }],
+            as: 'comments'
           }
         }
       ]
